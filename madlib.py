cbc03ab6c3668ed906682bc62aa0988d4b009ac0
Kagome Miller <kagomemiller@gmail.com>
2019-10-08 13:31:26 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,18 @@
+
+adverb = input('Give me an adverb...')
+object = input('Name an object...')
+
+output = f"She {adverb} ran down the street with a {object} in her hand."
+
+
+noun = input('Give me a noun...')
+object = input('Name an object...')
+output = f"The {noun} picked up the {object} and floated off to space!"
+
+
+
+location =input('Name a location...')
+sobject = input('Name a small object...')
+lobject = input('Name a large object...')
+
+output = f"If you follow the Yellow Brick road, you end up in {location} and you find {sobject}’s that you can use for building a {lobject}."
